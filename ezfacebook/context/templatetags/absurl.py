9c08b01ee5cf75c3589c84df7d14ed99dc6dc499
Evan Leis <evan.leis@wearetopsecret.com>
2011-12-01 16:14:03 -0700

    Making ezfacebook.context an app

--- ezfacebook/context/templatetags/absurl.py
+++ ezfacebook/context/templatetags/absurl.py
@@ -2,7 +2,6 @@ from django import template
 from django.contrib.sites.models import Site
 from django.template.defaultfilters import stringfilter
 
-
 register = template.Library()
 
 @register.filter
--- ezfacebook/example_settings.py
+++ ezfacebook/example_settings.py
@@ -1,37 +1,33 @@
 
-
 TEMPLATE_CONTEXT_PROCESSORS = (
-    'ezfacebook.context.context_processors.facebook', # Puts FACEBOOK_SETTINGS and FACEBOOK_CHANNEL_URL in template context
+    'ezfacebook.context.context_processors.facebook', # ezfacebook.context: Requirement
 )
 
 MIDDLEWARE_CLASSES = (
-    'ezfacebook.helpers.middleware.IEIFrameApplicationMiddleware' # Required for IE in iframe environments (example Page Tab) if sessions are to work.
+    'ezfacebook.helpers.middleware.IEIFrameApplicationMiddleware' # ezfacebook.helpers: Optional
 )
 
 INSTALLED_APPS = (
-    # Nothing special needs to be installed to use this package
+    'django.contrib.sites', # ezfacebook.context: Requirement
+    'ezfacebook.context', # ezfacebook.context: Requirement
 )
 
-class FACEBOOK_SETTINGS:
+class FACEBOOK_SETTINGS: # ezfacebook.user: Requirement
 
     class my_first_fb_app:
         app_id = '00000000000000',
         secret = 'abcdef0123456789',
         scope = 'email,publish_stream,offline_access'
-        # Enable debug mode for ezfacebook.user.decorators.parse_signed_request
-        debug_signed_request = {'id': 23840238402834}
-        # Enable debug mode for ezfacebook.user.decorators.graph_from_cookies
+
+        debug_signed_request = {'id': 23840238402834} # 
         debug_guid = 23840238402834
-        # Enable debug mode for ezfacebook.user.decorators.graph_from_cookies
         debug_token = 'AAAAAAAAAbbbbbbbbbbccccdefffffffffffffffffffetc'
 
     class my_second_fb_app:
         app_id = '11111111111111',
         secret = '9876543210abcdef',
         scope = ''
-        # Disable debug mode for ezfacebook.user.decorators.parse_signed_request
+
         debug_signed_request = False
-        # Disable debug mode for ezfacebook.user.decorators.graph_from_cookies
         debug_guid = False
-        # Disable debug mode for ezfacebook.user.decorators.graph_from_cookies
         debug_token = False
