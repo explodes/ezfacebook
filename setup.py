92043295d9b457d7747437d87195896779842062
Evan Leis <evan.leis@wearetopsecret.com>
2011-12-02 13:20:56 -0700

    Somehow setup.py got deleted

---
+++ setup.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+#-----------------------------------------------------------------------------
+# Copyright (c) 2011, Evan Leis
+#
+# Distributed under the terms of the Lesser GNU General Public License (LGPL)
+#-----------------------------------------------------------------------------
+
+from setuptools import setup, find_packages
+
+with open('README.md') as readme:
+    long_description = readme.read()
+
+setup(
+    name='ezfacebook',
+    version='0.75.1',
+    description="Django Tools to use facebook seamlessly without having to build around it.",
+    long_description=long_description,
+    author='Evan Leis',
+    author_email='engineergod@yahoo.com',
+    url='',
+    install_requires=[
+        'django>=1.3'
+    ],
+    setup_requires=[
+    ],
+    packages=find_packages(exclude=[
+    ]),
+    include_package_data=True,
+    test_suite="ezfacebook.tests",
+    package_data={},
+    zip_safe=True,
+    entry_points="""
+    """,
+    license="Apache Software License",
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Development Status :: 4 - Beta",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Framework :: Django",
+    ],
+)
